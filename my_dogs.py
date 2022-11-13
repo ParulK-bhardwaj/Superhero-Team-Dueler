eef64efd95233a40858c350ade291aad872eb35f
Parul Bhardwaj <bhardwaj.parul88@gmail.com>
2022-11-12 20:25:44 -0500

    Dog class Stretch challenge

--- my_dogs.py
+++ my_dogs.py
@@ -3,3 +3,12 @@ from dog import Dog
 
 my_dog = Dog("Rex", "superdog")
 my_dog.bark()
+
+my_other_dog = Dog("Annie", "SuperDog")
+print(my_other_dog.name)
+
+dog_1 = Dog("Coco", "ChocoDog")
+dog_1.sit()
+
+dog_2 = Dog("Chico", "ChicoDog")
+dog_2.roll_over()
